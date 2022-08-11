2f68e4a61a81ab16493906c940fb098f5987093e
Santiago Lopera Naranjo <santiagoln09@gmail.com>
2022-08-10 19:17:33 -0500

    Three methods completed

--- maxima_subsuma.py
+++ maxima_subsuma.py
@@ -1,25 +1,33 @@
-""" def maxima_subsuma():
+def maxima_subsuma():
   data = [4, -3, 5, -2, -1, 2, 7, -2]
   cantidad = len(data)
   suma_actual = 0
   maxima_suma = 0
-
   for n in range(cantidad):
     for m in range(cantidad):
       suma_actual = 0
-      print(m)
-      for o in range(m):
-        o = n
-        suma_actual += data[o]
-      
+      for o in range(n, m+1):
+        suma_actual += data[o]      
       if(suma_actual > maxima_suma):
         maxima_suma = suma_actual
   
   print(maxima_suma)
-
-maxima_subsuma() """
-
+   
 def maxima_subsuma_dos():
+  data = [4, -3, 5, -2, -1, 2, 7, -2]
+  cantidad = len(data)
+  suma_actual = 0
+  maxima_suma = 0
+  for n in range(cantidad):
+      suma_actual = 0
+      for m in range(n):
+          suma_actual += data[m]
+          if(suma_actual > maxima_suma):
+             maxima_suma = suma_actual
+
+  print(maxima_suma)
+
+def maxima_subsuma_tres():
   data = [4, -3, 5, -2, -1, 2, 7, -2]
   cantidad = len(data)
   suma_actual = 0
@@ -33,9 +41,6 @@ def maxima_subsuma_dos():
   
   print(maxima_suma)
 
-
+maxima_subsuma()
 maxima_subsuma_dos()
-
-
-        
-        
+maxima_subsuma_tres()
