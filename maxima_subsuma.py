0b126b6889dbec4a496bdb05237029e96a09e074
Santiago Lopera Naranjo <santiagoln09@gmail.com>
2022-08-17 19:13:18 -0500

    Codigo con comentarios

--- maxima_subsuma.py
+++ maxima_subsuma.py
@@ -44,18 +44,26 @@ def maxima_subsuma_tres(data):
   print(f"Maxima subsuma 3: {maxima_suma}")
 
 
+
+"""El maximo del arreglo se estbalece en "limite", esto llena un array de la cantidad establecida 
+    desde -limite hasta limite, es decir si limite es 100, el array tendra 100 datos entre -100 y 1000    
+"""
 def llenado():
-    limit = 10000
+    limite = 1000
     data = []
     random_number = 0
-    for _ in range(limit):
-        random_number = random.randint(-limit,limit)
+    for _ in range(limite):
+        random_number = random.randint(-limite,limite)
         data.append(random_number)
     
     return data
 
+""" 
+Esta funcion ejecuta los tres metodos y mide los tiempos por cada uno asignandole el tiempo de ejecuion a un array llamado tiempos
+"""
 def analizarTiempo():
   data = llenado()
+  """ Funciones tiene los tres metodos a ser ejecutados """
   funciones = [maxima_subsuma,maxima_subsuma_dos,maxima_subsuma_tres]
   tiempos = []
   for funcion in funciones:
@@ -66,10 +74,3 @@ def analizarTiempo():
     tiempos.append(f"{total_time}ns")
   
   print(tiempos)
-
-
-analizarTiempo()
-
-
-
-
